14f38b6d0e014b6ab79a2ba31e90cbfc1dfdb248
samyak <samyakmhrzn9841@gmail.com>
2025-12-26 15:13:57 +0545

    remaps

--- nvim/lua/samyak/remap.lua
+++ nvim/lua/samyak/remap.lua
@@ -2,8 +2,14 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>r', '<C-r>')
 vim.keymap.set('n', '<leader>qq', vim.cmd.q)
+vim.keymap.set('n', '<leader>t', vim.cmd.NvimTreeToggle)
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP Code Action' })
+
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set('n', '<leader>dd', '"_dd')
+vim.keymap.set('v', '<leader>d', '"_d')
 
 -- Toggle line wrapping with <leader>w
 vim.keymap.set('n', '<leader>w', ':set wrap!<CR>', { desc = 'Toggle word wrap' })
@@ -12,3 +18,20 @@ vim.keymap.set('n', '<leader>w', ':set wrap!<CR>', { desc = 'Toggle word wrap' }
 -- 'linebreak' makes sure wrapping happens at words, not in the middle of them
 -- 'showbreak' displays a character at the start of a wrapped line
 vim.keymap.set('n', '<leader>L', ':set wrap! linebreak! showbreak=+++<CR>', { desc = 'Toggle word wrap with linebreak' })
+
+vim.diagnostic.config {
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = true,
+  },
+}
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
