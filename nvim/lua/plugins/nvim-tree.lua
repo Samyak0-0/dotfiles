0b299a2ba4af51c76facd16bbe48091def906de4
samyak <samyakmhrzn9841@gmail.com>
2026-03-27 18:32:17 +0545

    nvim tree custom keymaps

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,10 +6,26 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
+    local function my_on_attach(bufnr)
+      local api = require 'nvim-tree.api'
+
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+      -- api.map.on_attach.default(bufnr)
+
+      vim.keymap.set('n', 'c', api.tree.change_root_to_node, opts 'Change to Current Node Directory')
+      vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Toggle help')
+      vim.keymap.set('n', 'y', api.fs.copy.node, opts 'Copy File')
+      vim.keymap.set('n', 'gn', api.fs.copy.filename, opts 'Copy File Name')
+    end
     require('nvim-tree').setup {
       -- hijack_directories = {
       --   enable = false,
       -- },
+      on_attach = my_on_attach,
     }
   end,
 }
