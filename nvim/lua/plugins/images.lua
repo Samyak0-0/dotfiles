d8109b6f9833bf80d7b8e37af2f7a32065e669e7
samyak <samyakmhrzn9841@gmail.com>
2026-03-28 14:44:05 +0545

    images displayed in nvim using uerberzugpp

---
+++ nvim/lua/plugins/images.lua
@@ -0,0 +1,8 @@
+return {
+  '3rd/image.nvim',
+  opts = {
+    backend = 'ueberzug',
+    -- max_height_window_percentage = 50,
+    hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.svg' },
+  },
+}
