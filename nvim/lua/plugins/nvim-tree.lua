cdcf2462ec2ca4248c016966d29a982ac1016a9c
samyak <samyakmhrzn9841@gmail.com>
2026-07-20 23:25:35 +0545

    automatically close nvim tree after opening a file

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -20,12 +20,36 @@ return {
       vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Toggle help')
       vim.keymap.set('n', 'y', api.fs.copy.node, opts 'Copy File')
       vim.keymap.set('n', 'gn', api.fs.copy.filename, opts 'Copy File Name')
+
+      vim.keymap.set('n', '<CR>', function()
+        local node = api.tree.get_node_under_cursor()
+
+        api.node.open.edit()
+
+        if node.type == 'file' then
+          api.tree.close()
+        end
+      end, opts 'Open')
+      vim.keymap.set('n', 'v', function()
+        local node = api.tree.get_node_under_cursor()
+
+        api.node.open.vertical()
+
+        if node.type == 'file' then
+          api.tree.close()
+        end
+      end, opts 'open in vertical split and close')
     end
     require('nvim-tree').setup {
       -- hijack_directories = {
       --   enable = false,
       -- },
       on_attach = my_on_attach,
+      -- actions = {
+      --   open_file = {
+      --     quit_on_open = true,
+      --   },
+      -- },
     }
   end,
 }
