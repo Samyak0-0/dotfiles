796f9207e6b383c17ce762d669b27d234e8e6853
samyak <samyakmhrzn9841@gmail.com>
2026-08-08 20:06:02 +0545

    change telescopes format

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -47,11 +47,16 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        layout_config = {
+          height = 0.8,
+          prompt_position = 'top',
+        },
+        sorting_strategy = 'ascending',
+        -- mappings = {
+        --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        -- },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
